77bc15d695a19b95d74e94dba4c303d7dac7ac62
Bhumika Singal <bhumikasingal1310@gmail.com>
2024-08-22 17:10:53 +0530

    first flight card

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ var flights = [{
     departureTime: "18:35",
     arrivalTime: "21:05",
     flightDuration: "2h 30m",
-    flightFare: "4,426"
+    flightFare: "₹ 4,426"
 }];
 
 for (var i = 0; i < flights[0].airlineLogo.length; i++) {
     document.getElementById("airline-logo").classList.add(flights[0].airlineLogo[i]);
 }
+document.getElementById("airline-name").innerHTML = flights[0].airlineName;
+document.getElementById("source-city-code").innerHTML = (flights[0].sourceCityCode).concat(flights[0].sourceCity);
+document.getElementById("destination-city-code").innerHTML = (flights[0].destinationCityCode).concat(flights[0].destinationCity);
+document.getElementById("departure-time").innerHTML = flights[0].departureTime;
+document.getElementById("flight-duration").innerHTML = flights[0].flightDuration;
+document.getElementById("arrival-time").innerHTML = flights[0].arrivalTime;
+document.getElementById("flight-fare").innerHTML = flights[0].flightFare;
