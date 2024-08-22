9883bdebfbcd48e635db3d98cabdea5a684e8d06
Bhumika Singal <bhumikasingal1310@gmail.com>
2024-08-22 15:58:40 +0530

    developed the divisions according to the data

---
+++ script.js
@@ -0,0 +1,13 @@
+var flights = [{
+    airlineLogo: "fa-solid fa-tickets-airline",
+    airlineName: "Indigo",
+    sourceCityCode: "HYD",
+    sourceCity: "Hyderabad, India",
+    destinationCityCode: "DEL",
+    destinationCity: "New Delhi, India",
+    departureTime: "18:35",
+    arrivalTime: "21:05",
+    flightDuration: "2h 30m",
+    flightFare: "4,426"
+}];
+
